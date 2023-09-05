7f5b1c15d8177dd0dd667c1aa11535bbc41d2809
isaisaransig <isai.saransig@epn.edu.ec>
2023-09-05 18:42:57 -0500

    update

---
+++ 02 - Android/app/src/main/java/com/example/movilescomputacion2023a/ICities.kt
@@ -0,0 +1,14 @@
+package com.example.movilescomputacion2023a
+
+class ICities(
+    public var name: String?,
+    public var state: String?,
+    public var country: String?,
+    public var capital: Boolean?,
+    public var population: Long?,
+    public var regiones: List<String>?
+) {
+    override fun toString(): String {
+        return "${name} - ${country}"
+    }
+}
