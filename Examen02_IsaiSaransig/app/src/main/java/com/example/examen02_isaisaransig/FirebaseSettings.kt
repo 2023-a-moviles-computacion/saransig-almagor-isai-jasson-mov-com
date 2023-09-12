f4632ad1dd253c35fdc8f96707ad5cd01f8dc013
isaisaransig <isai.saransig@epn.edu.ec>
2023-09-12 14:43:48 -0500

    delete and update examn

--- Examen02_IsaiSaransig/app/src/main/java/com/example/examen02_isaisaransig/FirebaseSettings.kt
+++ Examen02_IsaiSaransig/app/src/main/java/com/example/examen02_isaisaransig/FirebaseSettings.kt
@@ -18,15 +18,15 @@ class FirebaseSettings {
 
     fun createTiendas(tienda: Tienda){
         val db = Firebase.firestore
-        val referenciaEjemploEstudiante = db
+        val tiendaRef = db
             .collection("tienda")
-        val datosEstudiante = hashMapOf(
+        val datosTienda = hashMapOf(
             "nombre" to tienda.nombre,
             "direccion" to tienda.direccion,
             "ciudad" to tienda.ciudad,
             "numeroEmpleados" to tienda.numeroEmpleados
         )
-        referenciaEjemploEstudiante.add(datosEstudiante)
+        tiendaRef.add(datosTienda)
 
     }
 
@@ -166,7 +166,70 @@ class FirebaseSettings {
             }
     }
 
+    fun deleteProducto(tienda: String, adaptador: ArrayAdapter<Producto>, descripcion: String?){
+        val db = Firebase.firestore
+        val tiendaRef = db.collection("tienda")
+        tiendaRef.whereEqualTo("nombre", tienda)
+            .get()
+            .addOnSuccessListener {
+                documents ->
+                for (document in documents){
+                    var productoRef = document.reference.collection("productos")
+                    productoRef.whereEqualTo("descripcion", descripcion)
+                        .get()
+                        .addOnSuccessListener {
+                            productos ->
+                            for (producto in productos){
+                                producto.reference.delete()
+                            }
 
+                            // Elimina el elemento del arreglo y notifica al adaptador
+                            val index = arregloProductos.indexOfFirst { it.descripcion == descripcion }
+                            if (index != -1) {
+                                arregloProductos.removeAt(index)
+                                adaptador.notifyDataSetChanged()
+                            }
+                        }
+                }
+            }
+    }
 
+    fun updateProducto(tienda: String, adaptador: ArrayAdapter<Producto>, producto: Producto){
+        val db = Firebase.firestore
+        val datosUpdate = hashMapOf(
+            "descripcion" to producto.descripcion as Any,
+            "fechaDeElaboracion" to producto.fechaDeElaboracion as Any,
+            "precio" to producto.precio as Any,
+            "descuento" to producto.descuento as Any
+        )
+
+        val tiendaRef = db.collection("tienda")
+        tiendaRef.whereEqualTo("nombre", tienda)
+            .get()
+            .addOnSuccessListener {
+                documents ->
+                for(document in documents){
+                    val productoRef = document.reference.collection("productos")
+                    productoRef.whereEqualTo("descripcion", producto.descripcion)
+                        .get()
+                        .addOnSuccessListener {
+                            productos ->
+                            for (product in productos){
+                                product.reference.update(datosUpdate)
+                                    .addOnSuccessListener {
+                                        val index = arregloProductos.indexOfFirst { it.descripcion == producto.descripcion}
+                                        if (index != -1) {
+                                            arregloProductos[index].fechaDeElaboracion = producto.fechaDeElaboracion
+                                            arregloProductos[index].precio = producto.precio
+                                            arregloProductos[index].descuento = producto.descuento
+                                            adaptador.notifyDataSetChanged()
+                                        }
+
+                                    }
+                            }
+                        }
+                }
+            }
+    }
 
 }
--- Examen02_IsaiSaransig/app/src/main/java/com/example/examen02_isaisaransig/ListProducto.kt
+++ Examen02_IsaiSaransig/app/src/main/java/com/example/examen02_isaisaransig/ListProducto.kt
@@ -84,7 +84,7 @@ class ListProducto : AppCompatActivity() {
             }
         }
 
-/*
+
     private var productoSeleccionado: Producto? = null
 
     override fun onCreateContextMenu(
@@ -98,7 +98,7 @@ class ListProducto : AppCompatActivity() {
         val info = menuInfo as AdapterView.AdapterContextMenuInfo
         val id = info.position
         idItemSeleccionado = id
-        productoSeleccionado = BDExamen.arregloProductos[info.position]
+        productoSeleccionado = FirebaseSettings.arregloProductos[info.position]
 
     }
 
@@ -110,11 +110,10 @@ class ListProducto : AppCompatActivity() {
                 return true
             }
             R.id.mi_eliminar -> {
-                val idOb = productoSeleccionado?.idProducto
+                val descripcion = productoSeleccionado?.descripcion
 
                 abrirDialogo {
-                    COBaseDatos.coBDatos!!.deleteProducto(idOb)
-                    COBaseDatos.coBDatos!!.readProductos(idTienda)
+                    COFirebase.firebase!!.deleteProducto(nombreTienda, adaptador, descripcion)
                     adaptador.notifyDataSetChanged()
                 }
 
@@ -123,6 +122,21 @@ class ListProducto : AppCompatActivity() {
             else -> super.onContextItemSelected(item)
         }
     }
+
+
+    fun abrirDialogo(callback: () -> Unit) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Desea eliminar")
+        builder.setPositiveButton(
+            "Aceptar"
+        ) { dialog, which ->
+            callback.invoke()
+        }
+        builder.setNegativeButton("Cancelar", null)
+        val dialogo = builder.create()
+        dialogo.show()
+    }
+
 
     fun irEditProducto(
         clase: Class <*>
@@ -146,34 +160,14 @@ class ListProducto : AppCompatActivity() {
                     val precio = result.data?.getDoubleExtra("precio", 0.0)
                     val descuento = result.data?.getIntExtra("descuento", 0)
                     val fechaElaboracion = result.data?.getLongExtra("fechaDeElaboracion", 0)
-                    COBaseDatos.coBDatos!!.updateProducto(
-                        productoSeleccionado?.idProducto,
-                        idTienda,
-                        descripcion,
-                        fechaElaboracion,
-                        precio,
-                        descuento
+                    val producto = Producto(descripcion,fechaElaboracion, precio, descuento )
+                    COFirebase.firebase!!.updateProducto(
+                        nombreTienda,
+                        adaptador,
+                        producto
                     )
-                    COBaseDatos.coBDatos!!.readProductos(idTienda)
-                    adaptador.notifyDataSetChanged()
                 }
             }
         }
 
-    fun abrirDialogo(callback: () -> Unit) {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Desea eliminar")
-        builder.setPositiveButton(
-            "Aceptar"
-        ) { dialog, which ->
-            callback.invoke()
-        }
-        builder.setNegativeButton("Cancelar", null)
-        val dialogo = builder.create()
-        dialogo.show()
-    }
-
-
-
-    */
 }
