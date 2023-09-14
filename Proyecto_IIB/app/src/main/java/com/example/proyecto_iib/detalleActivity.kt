d517b09a08ed40262600f1c1fec2279c37c1e243
isaisaransig <isai.saransig@epn.edu.ec>
2023-09-14 11:09:57 -0500

    detalle activity

---
+++ Proyecto_IIB/app/src/main/java/com/example/proyecto_iib/detalleActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_iib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detalleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle)
+    }
+}
