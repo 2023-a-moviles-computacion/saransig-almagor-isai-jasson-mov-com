6df877c4b17af051d807a304eb8941bce4f83321
isaisaransig <isai.saransig@epn.edu.ec>
2023-09-14 23:10:57 -0500

    detalle

--- Proyecto_IIB/app/src/main/java/com/example/proyecto_iib/detalleActivity.kt
+++ Proyecto_IIB/app/src/main/java/com/example/proyecto_iib/detalleActivity.kt
@@ -1,9 +1,12 @@
 package com.example.proyecto_iib
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 
 
 class detalleActivity : AppCompatActivity() {
@@ -11,6 +14,10 @@ class detalleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detalle)
+        val btn_catalogo = findViewById<ImageButton>(R.id.ibtn_catalogo)
+        btn_catalogo.setOnClickListener {
+            irActividad(catalogoActivity::class.java)
+        }
         codigo = intent.getStringExtra("codigo").toString()
         COFirebase.firebase!!.getRopa(codigo, object : RopaCallback {
             override fun onRopaObtenida(ropa: Ropa) {
@@ -18,17 +25,16 @@ class detalleActivity : AppCompatActivity() {
                 var precio = findViewById<TextView>(R.id.tv_precio_detalle)
                 var detalle = findViewById<TextView>(R.id.tv_detalle_detalle)
                 var ropaImg = findViewById<ImageView>(R.id.img_detalle)
+
                 tipo_detalle.text = ropa.tipo
-                precio.text = "USD " + ropa.precio
+                precio.text = ropa.precio.toString()
                 detalle.text = ropa.detalle
+                val imagenResourceId = resources.getIdentifier(ropa.imagen, "drawable", packageName)
 
-                val imageResource = view.itemView.context.resources.getIdentifier(
-                    ropaActual.imagen,
-                    "drawable",
-                    holder.itemView.context.packageName
-                )
+                if (imagenResourceId != 0) {
+                    ropaImg.setImageResource(imagenResourceId)
+                }
 
-                holder.ropaImg.setImageResource(imageResource)
 
             }
 
@@ -37,5 +43,14 @@ class detalleActivity : AppCompatActivity() {
             }
         })
 
+    }
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+        overridePendingTransition(0, 0);
+        finish()
     }
 }
